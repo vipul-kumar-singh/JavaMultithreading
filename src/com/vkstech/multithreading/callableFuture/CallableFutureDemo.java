12470d3a661c7ebbb2bf9b30cfaf1230b8842914
Vipul Kumar Singh <vipul24.9669@gmail.com>
2022-02-23 19:36:19 +0530

    Callable and Future

---
+++ src/com/vkstech/multithreading/callableFuture/CallableFutureDemo.java
@@ -0,0 +1,47 @@
+package com.vkstech.multithreading.callableFuture;
+
+import java.util.concurrent.*;
+
+public class CallableFutureDemo {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        System.out.println("Factorial Service called for 10!");
+        Future<Long> result10 = executor.submit(new FactorialService(10));
+
+        System.out.println("Factorial Service called for 20!");
+        Future<Long> result20 = executor.submit(new FactorialService(20));
+
+        Long factorial10 = result10.get();
+        System.out.println("10! = " + factorial10);
+
+        Long factorial20 = result20.get();
+        System.out.println("20! = " + factorial20);
+
+        executor.shutdown();
+    }
+}
+
+class FactorialService implements Callable<Long> {
+
+    private int number;
+
+    public FactorialService(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public Long call() throws Exception {
+        long result = 1;
+
+        while (number != 0) {
+            result = number * result;
+            number--;
+            Thread.sleep(100);
+        }
+        return result;
+
+    }
+}
+
